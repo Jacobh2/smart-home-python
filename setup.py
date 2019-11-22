e807d5aaafc261a4baba32bc785d22826c9129ea
Jacob Hagstedt <jacob.hagstedt@lamneasoftware.se>
2019-11-22 15:11:11 +0100

    Add missing requirement

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
-install_requires = ["google-api-python-client"]
+install_requires = [
+    "google-api-python-client",
+    "requests"
+]
 
 setup(
     name="smart_home_python",
-    version="0.1",
+    version="0.1.1",
     description="Smart Home Python Google Action Library",
     url="https://github.com/Jacobh2/smart-home-python",
     install_requires=install_requires,
